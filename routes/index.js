73d3c538f825cc9c54f6dbd73c0f5b1756b6e02a
mcpejs <mcpejs1@gmail.com>
2020-03-29 15:31:31 +0900

    페이징 기능 구현

--- routes/index.js
+++ routes/index.js
@@ -12,11 +12,33 @@ let ifAuthenticated = (req, res, next) => {
 }
 
 router.get('/',function(req,res){
-    var readallquery=`SELECT id,nickname,title,content,wtime FROM posts`
-    db.query(readallquery,function(err,posts,fields){
-        res.render('../views/main',{posts:posts})
+    // 페이지수 없이 접근한다면 1페이지로 보냅니다.
+    res.redirect('/list_post/1')
+})
+
+router.get('/list_post/:page',function(req,res){
+    let postperpage=10
+    let requestPage=req.params.page
+    db.query('select count(*) as postcount from posts;',function(err,data,fields){
+        // 마지막 페이지를 알기위한 게시물수
+        let postcount=data[0].postcount
+
+        // 게시물을 보기위한 마지막 페이지
+        let lastPage=Math.floor(postcount/postperpage)+1
+
+        let startPost
+        // 마지막 페이지 이상의 요청들은 모두 마지막 페이지를 조회한것으로 간주
+        if(lastPage<=requestPage){
+            startPost=postcount-postperpage
+        } else {
+            startPost=postperpage*requestPage-postperpage
+        }
+        
+        let readpostquery=`SELECT * FROM posts ORDER BY id desc LIMIT ${startPost},${postperpage}`
+        db.query(readpostquery,function(err,posts,fields){
+            res.render('../views/main',{posts:posts})
+        })
     })
-    
 })
 
 router.get('/profile', ifAuthenticated, function (req, res) {
@@ -30,9 +52,9 @@ router.get('/create_post',ifAuthenticated,function(req,res){
 })
 
 router.get('/view_post/:id',function(req,res){
-    var post;
-    var readsinglequery=`SELECT id,nickname,title,content,wtime FROM posts WHERE id=${req.params.id}`
-    var commentsquery=`SELECT id,nickname,content,wtime FROM comments WHERE postid=${req.params.id}`
+    let post;
+    let readsinglequery=`SELECT * FROM posts WHERE id=${req.params.id}`
+    let commentsquery=`SELECT * FROM comments WHERE postid=${req.params.id}`
     db.query(readsinglequery,function(err,postdata,fields){
         post=postdata[0]
         db.query(commentsquery,function(err,commentsdata,fields){
