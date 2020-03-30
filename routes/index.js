4aec01fb13a39e6f8d055112245836c709e78981
mcpejs <mcpejs1@gmail.com>
2020-03-30 21:48:51 +0900

    페이징기능 수정
    페이징 코드의 주석을 추가했고,
    페이징의 예외 처리등을 추가했습니다.

--- routes/index.js
+++ routes/index.js
@@ -11,32 +11,50 @@ let ifAuthenticated = (req, res, next) => {
     res.redirect('/login')
 }
 
-router.get('/',function(req,res){
+router.get('/', function (req, res) {
     // 페이지수 없이 접근한다면 1페이지로 보냅니다.
     res.redirect('/list_post/1')
 })
 
-router.get('/list_post/:page',function(req,res){
-    let postperpage=10
-    let requestPage=req.params.page
-    db.query('select count(*) as postcount from posts;',function(err,data,fields){
+router.get('/list_post/:page', function (req, res) {
+    let postperpage = 10
+
+    // 첫게시글은 0번째이기 때문에 요청 -1을 해줍니다
+    let requestPage = req.params.page - 1
+
+    // 게시판 페이지 번호는 숫자만 들어와야합니다.
+    if (isNaN(requestPage)) {
+        res.status(404)
+        res.send('<script type="text/javascript">alert("정상적인 접근이 아닙니다.");location.href="/"</script>')
+    }
+    db.query('select count(*) as postcount from posts;', function (err, data, fields) {
+
         // 마지막 페이지를 알기위한 게시물수
-        let postcount=data[0].postcount
+        let postcount = data[0].postcount
 
         // 게시물을 보기위한 마지막 페이지
-        let lastPage=Math.floor(postcount/postperpage)+1
+        let lastPage = Math.floor(postcount / postperpage)
 
-        let startPost
-        // 마지막 페이지 이상의 요청들은 모두 마지막 페이지를 조회한것으로 간주
-        if(lastPage<=requestPage){
-            startPost=postcount-postperpage
+        let startIndex
+        if (lastPage <= requestPage) {
+            // 만약 요청페이지가 마지막 페이지 이상이라면
+            // 마지막 페이지의 결과를 보여줍니다.
+            startIndex = postcount - postperpage
+        } else if (requestPage < 1) {
+            // 만약 요청페이지가 1페이지보다 작다면
+            // 1페이지의 결과를 보여줍니다.
+            startIndex = 0
         } else {
-            startPost=postperpage*requestPage-postperpage
+            // 위 조건에 모두 해당하지 않는다면
+            // 해당하는 페이지를 보여줍니다.
+            startIndex = postperpage * requestPage
         }
-        
-        let readpostquery=`SELECT * FROM posts ORDER BY id desc LIMIT ${startPost},${postperpage}`
-        db.query(readpostquery,function(err,posts,fields){
-            res.render('../views/main',{posts:posts})
+
+        let readpostquery = `SELECT * FROM posts ORDER BY id desc LIMIT ${startIndex},${postperpage}`
+        db.query(readpostquery, function (err, posts, fields) {
+            res.render('../views/main', {
+                posts: posts
+            })
         })
     })
 })
@@ -47,33 +65,42 @@ router.get('/profile', ifAuthenticated, function (req, res) {
     })
 })
 
-router.get('/create_post',ifAuthenticated,function(req,res){
+router.get('/create_post', ifAuthenticated, function (req, res) {
     res.sendFile(path.resolve('./views/create.html'))
 })
 
-router.get('/view_post/:id',function(req,res){
+router.get('/view_post/:id', function (req, res) {
     let post;
-    let readsinglequery=`SELECT * FROM posts WHERE id=${req.params.id}`
-    let commentsquery=`SELECT * FROM comments WHERE postid=${req.params.id}`
-    db.query(readsinglequery,function(err,postdata,fields){
-        post=postdata[0]
-        db.query(commentsquery,function(err,commentsdata,fields){
-            res.render('../views/view',{post:post,comments:commentsdata})
+    let readsinglequery = `SELECT * FROM posts WHERE id=${req.params.id}`
+    let commentsquery = `SELECT * FROM comments WHERE postid=${req.params.id}`
+    db.query(readsinglequery, function (err, postdata, fields) {
+        post = postdata[0]
+        db.query(commentsquery, function (err, commentsdata, fields) {
+            res.render('../views/view', {
+                post: post,
+                comments: commentsdata
+            })
         })
-        
+
     })
 })
 
-router.get('/update_post/:id',ifAuthenticated,function(req,res){
-    res.render('../views/update',{id:req.params.id})
+router.get('/update_post/:id', ifAuthenticated, function (req, res) {
+    res.render('../views/update', {
+        id: req.params.id
+    })
 })
 
-router.get('/delete_post/:id',ifAuthenticated,function(req,res){
-    res.render('../views/delete',{id:req.params.id})
+router.get('/delete_post/:id', ifAuthenticated, function (req, res) {
+    res.render('../views/delete', {
+        id: req.params.id
+    })
 })
 
-router.get('/delete_comment/:commentid',ifAuthenticated,function(req,res){
-    res.render('../views/deletecomment',{commentid:req.params.commentid})
+router.get('/delete_comment/:commentid', ifAuthenticated, function (req, res) {
+    res.render('../views/deletecomment', {
+        commentid: req.params.commentid
+    })
 })
 
 router.get('/login', function (req, res) {
